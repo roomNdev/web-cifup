68bd389875c8347695837ef41a5efe3634ee511e
Roman <romndev@gmail.com>
2024-05-05 01:45:22 -0300

    eliminated unused code spotify

--- src/components/homePage/Podcasts.tsx
+++ src/components/homePage/Podcasts.tsx
@@ -6,23 +6,23 @@ import { PiApplePodcastsLogoLight } from "react-icons/pi";
 import { FaSpotify } from 'react-icons/fa';
 
 function Podcasts() {
-    window.onSpotifyIframeApiReady = (IFrameAPI) => {
-      const element = document.getElementById('embed-iframe');
-      const options = {
-        width: '100%',
-        height: '160',
-        uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
-      };
-      const callback = (EmbedController) => {
-        document.querySelectorAll('.episode').forEach(
-          episode => {
-            episode.addEventListener('click', () => {
-              EmbedController.loadUri(episode.dataset.spotifyId)
-            });
-          })
-      };
-      IFrameAPI.createController(element, options, callback);
-    };
+    // window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    //   const element = document.getElementById('embed-iframe');
+    //   const options = {
+    //     width: '100%',
+    //     height: '160',
+    //     uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
+    //   };
+    //   const callback = (EmbedController) => {
+    //     document.querySelectorAll('.episode').forEach(
+    //       episode => {
+    //         episode.addEventListener('click', () => {
+    //           EmbedController.loadUri(episode.dataset.spotifyId)
+    //         });
+    //       })
+    //   };
+    //   IFrameAPI.createController(element, options, callback);
+    // };
   
 
   return (
