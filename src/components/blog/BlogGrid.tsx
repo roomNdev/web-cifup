2f233e7415adb6b196ecdb44cb8cbf173da83e01
Roman <romndev@gmail.com>
2024-05-20 01:56:28 -0300

    Hidden instagram requests

--- src/components/blog/BlogGrid.tsx
+++ src/components/blog/BlogGrid.tsx
@@ -12,18 +12,18 @@ function BlogGrid({ blogs, numberOfPages, currentPage }) {
     setFilteredBlogs(posts)
   } 
 
-  const token = 'IGQWRQOTdQR2cyUzJ5UHd1NVZATOUJzRjdUbVh0WXF5MHFOVUxWMjVyTW1QdkF3N3ZA4ZAzVVb0Q2b05BQk9sOW9abzBsbjRsS1ZADbWhEUS1aQXBxZAkNnVjlhSnhTeXJETGlDdldjUE9oSTRGRjF1RXNUaHpMU3FRb1EZD'
-    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${token}`
-    const [feed, setFeed] = useState<any>()
-    useEffect(() => {
-        const getData = async () => {
-            const res = await fetch(url)
-            // console.log(await res.json());
-            setFeed(await res.json())
-            // console.log( res.json());
-        }
-        getData()
-        }, [])
+  // const token = 'IGQWRQOTdQR2cyUzJ5UHd1NVZATOUJzRjdUbVh0WXF5MHFOVUxWMjVyTW1QdkF3N3ZA4ZAzVVb0Q2b05BQk9sOW9abzBsbjRsS1ZADbWhEUS1aQXBxZAkNnVjlhSnhTeXJETGlDdldjUE9oSTRGRjF1RXNUaHpMU3FRb1EZD'
+  //   const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${token}`
+  //   const [feed, setFeed] = useState<any>()
+  //   useEffect(() => {
+  //       const getData = async () => {
+  //           const res = await fetch(url)
+  //           // console.log(await res.json());
+  //           setFeed(await res.json())
+  //           // console.log( res.json());
+  //       }
+  //       getData()
+  //       }, [])
     //     const urlProfile = `https://graph.instagram.com/me?fields=id,username&access_token=IGQWRQekk2RGhWV1NBNTRXY202a0R4Ykw2cnJOSkJOT3RHT241Smk1LWxiWEd3Skt4dDV3RnFJWXQyV2lpaERwcWVpemFhczExSlJGVkRDa0ExcGFwMHFBS3Jsdk94TVo2QnFLS2l2THg5T3VCNElkU0JpWjJfUm8ZD`
     //     const [profile, setProfile] = useState()
     //     useEffect(() => {
@@ -37,11 +37,11 @@ function BlogGrid({ blogs, numberOfPages, currentPage }) {
     
 
         
-        console.log(feed);
-    //     console.log(profile);
-        const filterWord = ""
-        const filteredFeed = feed?.data.filter(i => i.media_type !== "VIDEO")
-        console.log(filteredFeed);
+    //     console.log(feed);
+    // //     console.log(profile);
+    //     const filterWord = ""
+    //     const filteredFeed = feed?.data.filter(i => i.media_type !== "VIDEO")
+    //     console.log(filteredFeed);
 
   return (
     <BlogGridStyles>
@@ -65,14 +65,15 @@ function BlogGrid({ blogs, numberOfPages, currentPage }) {
               publishedAt={blog.publishedAt}
             />
           ))
-          : filteredFeed.filter(i => i.caption.includes(filterWord)).map((item) => {
-                return <article className="ig-new">
-                    {/* <StaticImage alt='' src='../../images/bank-of-america-new-logo.jpg'></StaticImage> */}
-                    <img src={item.media_url} alt="" />
-                    {/* <h2>Evento junto con Bank of America</h2> */}
-                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eaque voluptatibus accusantium </p> */}
-                </article>
-            })
+          : 'Oops! todavia no hay noticias'
+      //     : filteredFeed.filter(i => i.caption.includes(filterWord)).map((item) => {
+      //           return <article className="ig-new">
+      //               {/* <StaticImage alt='' src='../../images/bank-of-america-new-logo.jpg'></StaticImage> */}
+      //               <img src={item.media_url} alt="" />
+      //               {/* <h2>Evento junto con Bank of America</h2> */}
+      //               {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eaque voluptatibus accusantium </p> */}
+      //           </article>
+      //       })
       }
       </div>
       
--- src/components/homePage/InstagramSection.tsx
+++ src/components/homePage/InstagramSection.tsx
@@ -15,18 +15,18 @@ import SimpleSlider from '../Slider';
   
 
 const InstagramSection: React.FC<{}> = () => {
-    const token = 'IGQWRPWWxfelJJM3Fxc1FaLVg1NFRtNjczeW1JSENNa3htYW45TWs1ZAUZAzZAlRJaGNSRFd2X0x4ZAHZAGNFh0N2Q0YU15WmVxbTIwX2VFeVdFeTR4QkIyTnotbjYzWFJJZAV92ZAFhtT0M2elgxSEVXWUEtUVU3ekk1M1kZD'
-    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${token}`
-    const [feed, setFeed] = useState<any>()
-    useEffect(() => {
-        const getData = async () => {
-            const res = await fetch(url)
-            // console.log(await res.json());
-            setFeed(await res.json())
-            // console.log( res.json());
-        }
-        getData()
-        }, [])
+    // const token = 'IGQWRPWWxfelJJM3Fxc1FaLVg1NFRtNjczeW1JSENNa3htYW45TWs1ZAUZAzZAlRJaGNSRFd2X0x4ZAHZAGNFh0N2Q0YU15WmVxbTIwX2VFeVdFeTR4QkIyTnotbjYzWFJJZAV92ZAFhtT0M2elgxSEVXWUEtUVU3ekk1M1kZD'
+    // const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${token}`
+    // const [feed, setFeed] = useState<any>()
+    // useEffect(() => {
+    //     const getData = async () => {
+    //         const res = await fetch(url)
+    //         // console.log(await res.json());
+    //         setFeed(await res.json())
+    //         // console.log( res.json());
+    //     }
+    //     getData()
+    //     }, [])
     //     const urlProfile = `https://graph.instagram.com/me?fields=id,username&access_token=IGQWRQekk2RGhWV1NBNTRXY202a0R4Ykw2cnJOSkJOT3RHT241Smk1LWxiWEd3Skt4dDV3RnFJWXQyV2lpaERwcWVpemFhczExSlJGVkRDa0ExcGFwMHFBS3Jsdk94TVo2QnFLS2l2THg5T3VCNElkU0JpWjJfUm8ZD`
     //     const [profile, setProfile] = useState()
     //     useEffect(() => {
@@ -40,53 +40,25 @@ const InstagramSection: React.FC<{}> = () => {
     
 
         
-        console.log(feed);
-    //     console.log(profile);
-        const filterWord = ""
-        const filteredFeed = feed?.data.filter(i => i.media_type !== "VIDEO")
-        console.log(filteredFeed);
+    //     console.log(feed);
+    // //     console.log(profile);
+    //     const filterWord = ""
+    //     const filteredFeed = feed?.data.filter(i => i.media_type !== "VIDEO")
+    //     console.log(filteredFeed);
 
     return (
         <InstagramSectionStyles>
-            <h1>Eventos</h1>
+            {/* <h1>Eventos</h1>
             <SimpleSlider>
                 {
                     filteredFeed && filteredFeed.filter(i => i.caption.includes(filterWord)).map((item) => {
                         return <article className="event demo-slide" key={item.permalink}>
-                            {/* <StaticImage alt='' src='../../images/bank-of-america-new-logo.jpg'></StaticImage> */}
                             <img src={item.media_url} alt="" />
-                            {/* <h2>Evento junto con Bank of America</h2> */}
-                            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eaque voluptatibus accusantium </p> */}
                         </article>
                     })
                 }
-                {/* <article className="event demo-slide">
-                    <StaticImage alt='' src='../../images/bank-of-america-new-logo.jpg'></StaticImage>
-                    <h2>Evento junto con Bank of America</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eaque voluptatibus accusantium </p>
-                </article>
-                <article className="event demo-slide">
-                    <StaticImage alt='' src='../../images/bank-of-america-new-logo.jpg'></StaticImage>
-                    <h2>Evento junto con Bank of America</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eaque voluptatibus accusantium </p>
-                </article>
-                <article className="event demo-slide">
-                    <StaticImage alt='' src='../../images/bank-of-america-new-logo.jpg'></StaticImage>
-                    <h2>Evento junto con Bank of America</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eaque voluptatibus accusantium </p>
-                </article>
-                <article className="event demo-slide">
-                    <StaticImage alt='' src='../../images/bank-of-america-new-logo.jpg'></StaticImage>
-                    <h2>Evento junto con Bank of America</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eaque voluptatibus accusantium </p>
-                </article>
-                <article className="event demo-slide">
-                    <StaticImage alt='' src='../../images/bank-of-america-new-logo.jpg'></StaticImage>
-                    <h2>Evento junto con Bank of America</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eaque voluptatibus accusantium </p>
-                </article> */}
 
-            </SimpleSlider>            
+            </SimpleSlider>             */}
         </InstagramSectionStyles>
     )
 }
